9511254b9b4d94dfcd3be0fcabf670ae5afe735b
ogersoy <oguzhan.ersoy.9530@student.uu.se>
2024-06-04 22:00:19 +0200

    Readme and made the boss significantly faster

--- Model/Boss.js
+++ Model/Boss.js
@@ -1,11 +1,19 @@
+/**
+ * Represents a Boss in the game.
+ */
 export class Boss {
+	/**
+	 * Creates a new instance of the Boss class.
+	 * @param {object} game - The game object.
+	 * @param {number} bossLives - The number of lives the boss has.
+	 */
 	constructor(game, bossLives) {
 		this.game = game;
 		this.width = 200;
 		this.height = 200;
 		this.x = this.game.width / 2 - this.width / 2;
 		this.y = -this.height;
-		this.speedX = Math.random() < 0.5 ? -1 : 1;
+		this.speedX = (Math.random() < 0.5 ? -1 : 1) * 2;
 		this.speedY = 0;
 		this.lives = bossLives;
 		this.maxLives = this.lives;
@@ -16,21 +24,24 @@ export class Boss {
 		this.maxFrame = 11;
 	}
 
+	/**
+	 * Updates the boss's position and checks for collisions.
+	 */
 	update() {
 		this.speedY = 0;
 		if (this.game.spriteUpdate && this.lives >= 1) this.frameX = 0;
-		if (this.y < 0) this.y += 4;
+		if (this.y < 0) this.y += 8; // Increase from 4 to 8 to double the speed
 		if (
 			this.x < 0 ||
 			(this.x > this.game.width - this.width && this.lives >= 1)
 		) {
 			this.speedX *= -1;
-			this.speedY = this.height * 0.5;
+			this.speedY = this.height;
 		}
 		this.x += this.speedX;
 		this.y += this.speedY;
 
-		this.game.projectilesPool.forEach((projectile) => {
+		this.game.projectilesPool.forEach((projectile) => {//Boss life updater
 			if (
 				this.game.checkCollision(this, projectile) &&
 				!projectile.free &&
@@ -41,12 +52,12 @@ export class Boss {
 			}
 		});
 
-		if (this.game.checkCollision(this, this.game.player) && this.lives >= 1) {
+		if (this.game.checkCollision(this, this.game.player) && this.lives >= 1) {//Boss collision with player, automatically kills the player.
 			this.game.gameOver = true;
 			this.lives = 0;
 		}
 
-		if (this.lives < 1 && this.game.spriteUpdate) {
+		if (this.lives < 1 && this.game.spriteUpdate) {//Check if the boss is dead or at the end every frame
 			this.frameX++;
 			if (this.frameX > this.maxFrame) {
 				this.beDeleted = true;
@@ -59,6 +70,10 @@ export class Boss {
 		if (this.y + this.height > this.game.height) this.game.gameOver = true;
 	}
 
+	/**
+	 * Applies damage to the boss.
+	 * @param {number} damage - The amount of damage to apply.
+	 */
 	hit(damage) {
 		this.lives -= damage;
 		if (this.lives >= 1) this.frameX = 1;
--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Space Invaders
-Simple Space Invaders game using Vanilla Js :)
-Use Vscode and liveserver to play, open the folder as a project in your ide of your choice and use the localserver to browser option.
+by; Moein Ghorbanali, Arina Shahbazi, Oguzhan Ersoy
+
+This is a Simple Space Invaders game using Vanilla Js :)
+
+We use Vscode and liveserver to play, but any JS web development enviroment that can run a local server would be fine.
+
+## Running the Game Using Live Server in VSCode, used the addon default instructions and added our notes to make it easier
+
+Step by step instructions in VSCode(in any OS, our team has 1 Windows user, 1 Mac user and 1 Linux user :)
+1. Open Visual Studio Code.
+
+2. If you haven't installed the Live Server extension, go to the Extensions view (Ctrl+Shift+X), search for `Live Server` by Ritwick Dey, and install it.
+
+3. Open the workspace or folder containing the game files in VSCode.
+
+4. Right-click on the `index.html` file in the Explorer view and select `Open with Live Server`. Alternatively, you can use the live server button in the status bar of VSCode(at the very bottom of the UI) while you are on any js or html file.
+
+5. A new browser window should open displaying the game. If it doesn't, open your preferred web browser and navigate to `http://127.0.0.1:5500/index.html`.
+
+6. Enjoy the game! If you want to stop the server, you can use the button in the status bar again.
+
+
